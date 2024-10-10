9d13078433a25652b01aae78de70cd9299e16d5f
2hwayoung <69628269+2hwayoung@users.noreply.github.com>
2024-10-10 22:00:27 +0900

    fix: completedCoupleMission Query Dto type (#43)

--- apps/functions/src/adapter/in/graphql/user/queries/couple/couple.dto.ts
+++ apps/functions/src/adapter/in/graphql/user/queries/couple/couple.dto.ts
@@ -1,11 +1,13 @@
 import { Field, ArgsType } from '@nestjs/graphql';
-import { IsDefined, IsNumber } from 'class-validator';
+import { IsDefined } from 'class-validator';
 import { GraphQLBigInt } from 'graphql-scalars';
+
+import { IsBigInt } from '@/common';
 
 @ArgsType()
 export class CoupleRequest {
   @Field(() => GraphQLBigInt)
   @IsDefined()
-  @IsNumber()
+  @IsBigInt()
   coupleId: bigint;
 }
