78c4f9b11ff29263bdacd8f7129a46aa09447bd4
WooMinGy <alsrl1110@naver.com>
2024-10-17 04:50:27 +0900

    feat/add feedback inquiry url

--- apps/native/src/screens/authenticated/primary/primary-my-page/primary-my-page.const.ts
+++ apps/native/src/screens/authenticated/primary/primary-my-page/primary-my-page.const.ts
@@ -1,2 +1,8 @@
 export const DELETE_USER_DIALOG_TITLE = '진짜 탈퇴하시렵니까?\n그동안의 기록이 전부 사라져요😢';
 export const SIGN_OUT_DIALOG_TITLE = '로그아웃 하시겠습니까?\n연인과의 질문에 답변할 수 없어요😢';
+
+export const MY_PAGE_INQUIRY_URL = 'https://tawk.to/chat/670ff7554304e3196ad2bbf6/1iab5s9ok';
+export const MY_PAGE_POLICY_URL =
+  'https://www.notion.so/kyewmw/TaleUs-11068bc12aea80288ff4e6189d366e9e';
+export const MY_PAGE_TERMS_URL =
+  'https://www.notion.so/kyewmw/TaleUs-11068bc12aea80c7ac3ddb9450170323';
